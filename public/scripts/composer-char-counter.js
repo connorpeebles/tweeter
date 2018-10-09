c09c181a0b7fed87de6f124a2f049cf4cc1dcae8
Connor Peebles <connorpeebles93@gmail.com>
2018-10-09 18:42:40 +0000

    changed the keydown and keypress functions to one bind function which can track any change to the input in the textarea (including cutting, pasting, etc)

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,6 @@
-let charCount = 140;
-
 $(document).ready(function() {
-  $(".new-tweet textarea").keypress(function() {
-    charCount--;
-    console.log(charCount);
-  });
-});
-
-$(document).ready(function() {
-  $(".new-tweet textarea").keydown(function(e) {
-    if (e.keyCode === 8 && charCount < 140) {
-      charCount++;
-      console.log(charCount);
-    }
+  $(".new-tweet textarea").bind("input propertychange", function() {
+    let tweepLength = $(this).val().length;
+    console.log(140 - tweepLength);
   });
 });
