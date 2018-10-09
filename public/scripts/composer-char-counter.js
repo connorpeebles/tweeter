7cf8d4c1d9b1dc840a6fc849e9a5f6a8be2906c8
Connor Peebles <connorpeebles93@gmail.com>
2018-10-09 19:12:44 +0000

    updated the counter to appear red if it is negative

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,5 +2,10 @@ $(document).ready(function() {
   $(".new-tweet textarea").bind("input propertychange", function() {
     let charsLeft = 140 - $(this).val().length;
     $(this).siblings(".counter").text(charsLeft);
+    if (charsLeft < 0) {
+      $(this).siblings(".counter").css({color: "red"});
+    } else {
+      $(this).siblings(".counter").css({color: "#244751"});
+    }
   });
 });
